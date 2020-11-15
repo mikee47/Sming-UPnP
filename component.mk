e5ed67a4e36df20fbfa1e96cfd3527dce8ee1f39
mikee47 <mike@sillyhouse.net>
2020-11-15 17:22:57 +0000

    Add Python requirements

--- component.mk
+++ component.mk
@@ -2,6 +2,8 @@ COMPONENT_DEPENDS := SSDP RapidXML
 COMPONENT_INCDIRS := src/include
 COMPONENT_SRCDIRS := src
 
+COMPONENT_PYTHON_REQUIREMENTS := requirements.txt
+
 UPNP_TOOLS		:= $(COMPONENT_PATH)/tools
 
 UPNP_GENERATE_DEVICE = $(python) $(UPNP_TOOLS)/gen.py -t $(UPNP_TOOLS)/xsl/device.xsl
