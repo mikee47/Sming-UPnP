68dc2c71cbbd543449303b77b1b7395b50e83255
mikee47 <mike@sillyhouse.net>
2020-11-17 08:53:38 +0000

    Add Sony HG1 renderer (I don't have a viera!) so we get correct endpoint URLs.
    So we _are_ going to need device descriptions to support generic controls.

--- tools/gen.py
+++ tools/gen.py
@@ -11,14 +11,17 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description='UPnP Code Generator')
     parser.add_argument('-t', '--transform', metavar='filename', required=True, help='XSLT transform file')
-    parser.add_argument('-i', '--input', metavar='filename', required=True, help='Source .xml')
+    parser.add_argument('-b', '--base', metavar='filename', required=True, help='Base source directory')
+    parser.add_argument('-i', '--input', metavar='filename', required=True, help='Relative path to device description .xml')
     parser.add_argument('-o', '--output', metavar='filename', required=True, help='Destination .h')
     args = parser.parse_args()
 
-    original_tree = etree.parse(args.input)
+    src = os.path.join(args.base, args.input)
+    src = src.replace("\\", "/")
+    original_tree = etree.parse(src)
     xslt_tree = etree.parse(args.transform)
     xslt = etree.XSLT(xslt_tree)
-    doc_root = "'{}/config'".format(os.path.relpath(".", os.path.dirname(args.transform)))
+    doc_root = "'{}'".format(os.path.relpath(args.base, os.path.dirname(args.transform)))
     doc_root = doc_root.replace("\\", "/")
     lom_tree = xslt(original_tree, DOC_ROOT=doc_root)
 
