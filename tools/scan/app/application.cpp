4a18fe3321c83a176984d5fd0c8faab825ba200d
mikee47 <mike@sillyhouse.net>
2020-11-18 18:07:27 +0000

    Output device schema by manufacturer/friendlyName/deviceType
    Device implementations vary so only services are common

--- tools/scan/app/application.cpp
+++ tools/scan/app/application.cpp
@@ -1,5 +1,7 @@
 #include <SmingCore.h>
 #include <Network/UPnP/ControlPoint.h>
+#include <Data/BitSet.h>
+#include <Data/CString.h>
 
 #ifdef ARCH_HOST
 #include <io.h>
@@ -15,40 +17,191 @@
 namespace
 {
 NtpClient* ntpClient;
-UPnP::ControlPoint controlPoint(8192);
+UPnP::ControlPoint controlPoint(65536);
 Timer queueTimer;
 Timer statusTimer;
+Timer fetchTimer;
+
+void beginNextSearch();
+void fetchNextDescription();
+
+enum class Option {
+	networkTree,
+};
+
+BitSet<uint32_t, Option> options = Option::networkTree;
 
 constexpr unsigned maxDescriptionFetchAttempts{3};
+constexpr unsigned descriptionFetchTimeout{5000};
 
 // Fetch one description file at a time to avoid swamping the TCP stack
 struct Fetch {
+	UPnP::Urn::Kind kind{};
 	String url;
 	String root;
 	String path;
 	unsigned attempts{0};
 
+	Fetch() = default;
+	Fetch(const Fetch&) = default;
+
+	Fetch(const String& url, const String& root, const String& path)
+		: kind(UPnP::Urn(path).kind), url(url), root(root), path(path)
+	{
+	}
+
+	Fetch(const UPnP::Urn& urn) : kind(urn.kind), path(urn.toString())
+	{
+	}
+
+	explicit operator bool() const
+	{
+		return kind != UPnP::Urn::Kind::none;
+	}
+
 	bool operator==(const Fetch& other)
 	{
 		return path == other.path;
 	}
+
+	UPnP::Urn urn() const
+	{
+		return UPnP::Urn(path);
+	}
+
+	String fullPath() const
+	{
+		String s;
+		s += root;
+		s += path;
+		return s;
+	}
+
+	String toString() const
+	{
+		String s;
+		if(url) {
+			s += url;
+			s += F("' -> '");
+			s += root;
+			s += path;
+			s += '\'';
+		} else {
+			s = path;
+		}
+		return s;
+	}
 };
 
-Vector<Fetch> descriptionFetchList;
-Vector<Fetch> descriptionDoneList;
-Vector<String> ssdpFetchList;
-Vector<String> ssdpDoneList;
-
-void queueSsdp(const String& urn)
+class FetchList
 {
-	if(!ssdpDoneList.contains(urn) && !ssdpFetchList.contains(urn)) {
-		Serial.print(_F("Queuing SSDP "));
-		Serial.println(urn);
-		ssdpFetchList.add(urn);
+public:
+	FetchList(const String& name) : name(name)
+	{
 	}
-}
 
-static const char* baseOutputDir = "devices";
+	bool contains(const Fetch& f)
+	{
+		return queue.contains(f) || done.contains(f);
+	}
+
+	bool isDone(const Fetch& f)
+	{
+		return done.contains(f);
+	}
+
+	bool add(Fetch f)
+	{
+		if(f.kind > UPnP::Urn::Kind::service) {
+			f.kind = UPnP::Urn::Kind::none;
+		}
+
+		if(contains(f)) {
+			return false;
+		}
+
+		if(f.kind == UPnP::Urn::Kind::service) {
+			//			unsigned i = 0;
+			//			while(i < queue.count()) {
+			//				if(queue[i].)
+			//			}
+			queue.insertElementAt(f, 0);
+		} else {
+			queue.add(f);
+		}
+
+		Serial.print(_F("Queuing '"));
+		Serial.println(f.toString());
+
+		return true;
+	}
+
+	Fetch& peek()
+	{
+		if(queue.count() == 0) {
+			nil = Fetch();
+			return nil;
+		} else {
+			return queue[0];
+		}
+	}
+
+	Fetch pop()
+	{
+		if(queue.count() == 0) {
+			return Fetch{};
+		}
+
+		Fetch f = queue.at(0);
+		queue.remove(0);
+		return f;
+	}
+
+	void finished(const Fetch& f)
+	{
+		done.add(f);
+		queue.removeElement(f);
+	}
+
+	void finished(const String& url)
+	{
+		Fetch f;
+		f.url = url;
+		finished(f);
+	}
+
+	unsigned count() const
+	{
+		return queue.count();
+	}
+
+	String toString() const
+	{
+		String s;
+		s += name;
+		s += F(": fetched ");
+		s += done.count();
+		s += F(" of ");
+		s += done.count() + queue.count();
+		return s;
+	}
+
+private:
+	using List = Vector<Fetch>;
+	static Fetch nil;
+	String name;
+	List queue;
+	List done;
+};
+
+Fetch FetchList::nil{};
+
+FetchList descriptionQueue("Descriptions");
+FetchList ssdpQueue("SSDP messages");
+Fetch currentDesc;
+
+static const char* deviceDir = "devices";
+static const char* schemaDir = "schema";
 
 //IMPORT_FSTR(gatedesc_xml, PROJECT_DIR "/devices1/192.168.1.254/1900/gatedesc.xml")
 
@@ -72,16 +225,14 @@ void makedirs(const String& path)
 #endif
 }
 
-void checkPath(String& path)
+Print* openStream(String path)
 {
+#ifdef ARCH_HOST
 	path.replace(':', '-');
 	path.replace('?', '-');
 	path.replace('&', '-');
-}
+	path.replace(' ', '_');
 
-Print* openStream(const String& path)
-{
-#ifdef ARCH_HOST
 	makedirs(path);
 	Serial.print(_F("Writing "));
 	Serial.println(path);
@@ -95,57 +246,77 @@ Print* openStream(const String& path)
 #endif
 }
 
-void beginNextSearch();
-void fetchNextDescription();
-
-void queueDescription(const String& location, const String& root)
+void writeServiceSchema(XML::Document& scpd, const String& serviceType)
 {
-	Url url(location);
-	Fetch f{location, root, url.getRelativePath()};
-	checkPath(f.path);
-	if(descriptionFetchList.contains(f)) {
-		debug_d("%s already queued", location.c_str());
-		return;
+	UPnP::Urn urn(serviceType);
+	String path = schemaDir;
+	path += "/service/";
+	path += urn.domain;
+	path += '/';
+	path += urn.type;
+	path += urn.version;
+	path += ".xml";
+	auto fs = openStream(path);
+	if(fs != nullptr) {
+		XML::serialize(scpd, *fs, true);
+		delete fs;
 	}
-	if(descriptionDoneList.contains(f)) {
-		debug_d("%s already fetched", location.c_str());
-		return;
-	}
+}
 
-	descriptionFetchList.add(f);
+void writeDeviceSchema(XML::Node* device, const String& deviceType)
+{
+	auto node = device->first_node("manufacturer");
+	assert(node != nullptr);
+	String manufacturer(node->value(), node->value_size());
 
-	Serial.print(_F("Queuing '"));
-	Serial.print(location);
-	Serial.print(" (");
-	Serial.print(descriptionFetchList.count());
-	Serial.println(')');
+	node = device->first_node("friendlyName");
+	assert(node != nullptr);
+	String friendlyName(node->value(), node->value_size());
 
-	if(descriptionFetchList.count() == 1) {
-		queueTimer.initializeMs<2000>(fetchNextDescription).startOnce();
+	UPnP::Urn urn(deviceType);
+	String path = schemaDir;
+	path += "/device/";
+	path += manufacturer;
+	path += '/';
+	path += friendlyName;
+	path += '/';
+	path += urn.type;
+	path += urn.version;
+	path += ".xml";
+	auto fs = openStream(path);
+	if(fs != nullptr) {
+		XML::serialize(*device, *fs, true);
+		delete fs;
 	}
 }
 
 void parseDevice(XML::Node* device, const Fetch& f)
 {
-	String deviceType = device->first_node("deviceType")->value();
+	auto node = device->first_node("deviceType");
+	assert(node != nullptr);
+	String deviceType(node->value(), node->value_size());
 	debug_e("deviceType %sfound", deviceType ? "" : "NOT ");
-	queueSsdp(deviceType);
+	ssdpQueue.add(UPnP::Urn{deviceType});
+
+	writeDeviceSchema(device, deviceType);
 
 	auto serviceList = device->first_node("serviceList");
 	debug_i("serviceList %sfound", serviceList ? "" : "NOT ");
 	if(serviceList != nullptr) {
 		auto svc = serviceList->first_node();
 		while(svc != nullptr) {
-			String svcType = svc->first_node("serviceType")->value();
-			queueSsdp(svcType);
+			node = svc->first_node("serviceType");
+			assert(node != nullptr);
+			String svcType(node->value(), node->value_size());
+			ssdpQueue.add(UPnP::Urn{svcType});
 
 			auto node = svc->first_node("SCPDURL");
 			if(node == nullptr) {
 				debug_e("*** SCPDURL missing ***");
 			} else {
 				Url url(f.url);
-				url.Path = node->value();
-				queueDescription(String(url), f.root);
+				url.Path = String(node->value(), node->value_size());
+				descriptionQueue.add(Fetch(String(url), String(f.root), svcType));
 			}
 
 			svc = svc->next_sibling();
@@ -163,32 +334,47 @@ void parseDevice(XML::Node* device, const Fetch& f)
 	}
 }
 
-void parseDescription(XML::Document& doc, const Fetch& f)
+void parseDescription(XML::Document& description, const Fetch& f)
 {
-	auto device = XML::getNode(doc, "/device");
-	debug_e("device %sfound", device ? "" : "NOT ");
-	if(device != nullptr) {
-		parseDevice(device, f);
+	if(f.kind == UPnP::Urn::Kind::service) {
+		writeServiceSchema(description, f.path);
+	} else {
+		auto device = XML::getNode(description, "/device");
+		debug_e("device %sfound", device ? "" : "NOT ");
+		if(device != nullptr) {
+			parseDevice(device, f);
+		}
 	}
+}
+
+void scheduleFetch(unsigned delay = 2000)
+{
+	queueTimer.initializeMs(delay, fetchNextDescription).startOnce();
 }
 
 void fetchNextDescription()
 {
-	if(descriptionFetchList.count() == 0) {
-		queueTimer.initializeMs<2000>(beginNextSearch).startOnce();
-		return;
+	Fetch fetch;
+
+	for(;;) {
+		if(descriptionQueue.count() == 0) {
+			beginNextSearch();
+			return;
+		}
+
+		Fetch& f = descriptionQueue.peek();
+		++f.attempts;
+		if(f.attempts <= maxDescriptionFetchAttempts) {
+			Serial.print(_F("Fetching '"));
+			Serial.println(f.toString());
+			fetch = f;
+			break;
+		}
+		debug_e("Giving up on '%s' after %u attempts", f.toString().c_str(), f.attempts);
+		descriptionQueue.finished(f);
 	}
 
-	Fetch f = descriptionFetchList.at(0);
-
-	Serial.print(_F("Fetching '"));
-	Serial.print(f.url);
-	Serial.print(_F("' to '"));
-	Serial.print(f.root);
-	Serial.print(f.path);
-	Serial.println('\'');
-
-	auto callback = [](HttpConnection& connection, XML::Document* description) {
+	auto callback = [fetch](HttpConnection& connection, XML::Document* description) {
 #if DEBUG_VERBOSE_LEVEL == DBG
 		Serial.println();
 		Serial.println();
@@ -213,110 +399,112 @@ void fetchNextDescription()
 		}
 #endif
 
-		Fetch f = descriptionFetchList.at(0);
-		descriptionFetchList.remove(0);
+		auto& f = fetch;
 
 		if(!connection.getResponse()->isSuccess()) {
 			// Fetch failed, move to end of queue
-			++f.attempts;
 			if(f.attempts >= maxDescriptionFetchAttempts) {
-				debug_e("Giving up on '%s' after %u attempts", f.attempts);
+				debug_e("Giving up on '%s' after %u attempts", f.toString().c_str(), f.attempts);
+				descriptionQueue.finished(f);
 			} else {
 				Serial.print(_F("Fetch '"));
 				Serial.print(f.url);
-				Serial.println("' failed, re-queueing");
-				descriptionFetchList.add(f);
+				Serial.println("' failed, re-trying");
 			}
 		} else if(description != nullptr) {
-			// Write description
-			auto fs = openStream(f.root + f.path);
-			if(fs != nullptr) {
-				XML::serialize(*description, *fs, true);
-				delete fs;
+			if(options[Option::networkTree]) {
+				// Write description
+				auto fs = openStream(f.fullPath());
+				if(fs != nullptr) {
+					XML::serialize(*description, *fs, true);
+					delete fs;
+				}
 			}
 
-			descriptionDoneList.add(f);
+			descriptionQueue.finished(f);
 
 			parseDescription(*description, f);
 		}
 
-		queueTimer.initializeMs<1000>(fetchNextDescription).startOnce();
+		scheduleFetch();
 	};
 
-	unsigned due = 2000;
-	if(controlPoint.requestDescription(f.url, callback)) {
-		due = 10000;
+	if(controlPoint.requestDescription(fetch.url, callback)) {
+		fetchTimer.initializeMs<descriptionFetchTimeout>(fetchNextDescription);
+		fetchTimer.startOnce();
+	} else {
+		scheduleFetch();
 	}
-	queueTimer.initializeMs(due, fetchNextDescription).startOnce();
 }
 
 void onSsdp(SSDP::BasicMessage& msg)
 {
 	String location = msg[HTTP_HEADER_LOCATION];
 
-	if(ssdpDoneList.indexOf(location) >= 0) {
+	Fetch f;
+	f.url = location;
+
+	if(ssdpQueue.contains(f)) {
 		debug_d("%s - ignoring, already processed", location.c_str());
 		return;
 	}
 
 	// Create root directory for device
-	String root = baseOutputDir;
+	String root = deviceDir;
 	root += '/';
 	Url url(location);
 	root += url.Host;
 	root += '/';
 	root += url.Port;
 	root += '/';
-	/*
-	String path = url.getRelativePath();
-	int sep = path.lastIndexOf('/');
-	if(sep >= 0) {
-		root.concat(path.c_str(), sep);
-		root += '/';
-	}
-*/
-	makedirs(root);
 
-	// Write SSDP response
-	auto devtype = msg["ST"];
-	if(devtype == nullptr) {
-		devtype = msg["NT"];
-	}
-	String filename;
-	filename += "ssdp-";
-	filename += devtype;
-	filename += ".txt";
-	checkPath(filename);
+	f.root = root;
+	f.path = url.getRelativePath();
 
-	auto fs = openStream(root + filename);
-	if(fs != nullptr) {
-		fs->print(F("Message type: "));
-		fs->println(toString(msg.type));
-		fs->println();
-		for(unsigned i = 0; i < msg.count(); ++i) {
-			fs->print(msg[i]);
+	auto deviceType = msg["ST"];
+	if(deviceType == nullptr) {
+		deviceType = msg["NT"];
+	}
+
+	UPnP::Urn urn(deviceType);
+	f.kind = urn.kind;
+
+	if(options[Option::networkTree]) {
+		// Write SSDP response
+		String filename;
+		filename += "ssdp-";
+		filename += deviceType;
+		filename += ".txt";
+
+		auto fs = openStream(root + filename);
+		if(fs != nullptr) {
+			fs->print(F("Message type: "));
+			fs->println(toString(msg.type));
+			fs->println();
+			for(unsigned i = 0; i < msg.count(); ++i) {
+				fs->print(msg[i]);
+			}
+			delete fs;
 		}
-		delete fs;
 	}
 
-	ssdpDoneList.add(location);
+	ssdpQueue.finished(f);
 
-	queueDescription(location, root);
+	descriptionQueue.add(f);
+	scheduleFetch();
 }
 
 void beginNextSearch()
 {
 	controlPoint.cancelSearch();
-	if(ssdpFetchList.count() == 0) {
+	Fetch f = ssdpQueue.pop();
+	if(!bool(f)) {
 		Serial.println(_F("ALL DONE"));
 		System.restart(2000);
 	}
 
-	String urn = ssdpFetchList.at(0);
-	ssdpFetchList.remove(0);
-
-	controlPoint.beginSearch(UPnP::Urn(urn), onSsdp);
-	ssdpDoneList.add(urn);
+	controlPoint.beginSearch(f.urn(), onSsdp);
+	ssdpQueue.finished(f);
 }
 
 /*
@@ -341,21 +529,15 @@ Fetch and write each file with path.
  */
 void initUPnP()
 {
-	ssdpFetchList.add(String(UPnP::RootDeviceUrn()));
+	ssdpQueue.add(UPnP::RootDeviceUrn());
 	beginNextSearch();
 
 	statusTimer.initializeMs<10000>(InterruptCallback([]() {
 		Serial.println();
 		Serial.println();
 		Serial.println(_F("** Queue status **"));
-		Serial.print(_F("  Descriptions: fetched "));
-		Serial.print(descriptionDoneList.count());
-		Serial.print(_F(" of "));
-		Serial.println(descriptionDoneList.count() + descriptionFetchList.count());
-		Serial.print(_F("  SSDP:         fetched "));
-		Serial.print(ssdpDoneList.count());
-		Serial.print(_F(" of "));
-		Serial.println(ssdpDoneList.count() + ssdpFetchList.count());
+		Serial.println(descriptionQueue.toString());
+		Serial.println(ssdpQueue.toString());
 		Serial.println(_F("** ------------ **"));
 		Serial.println();
 		Serial.println();
