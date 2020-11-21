c1f374c14a75e069d8126c86c27bce9e9e1040f9
mikee47 <mike@sillyhouse.net>
2020-11-21 15:45:53 +0000

    Add tool make targets

--- component.mk
+++ component.mk
@@ -78,12 +78,28 @@ endif
 UPNP_SCAN_TOOL = $(UPNP_TOOLS)/scan/out/Host/debug/firmware/scan$(TOOL_EXT)
 
 .PHONY: upnp-tools
-upnp-tools: $(UPNP_SCAN_TOOL) ##Build UPnP tools - append SMING_ARCH as required
+upnp-tools: ##Build UPnP tools - append SMING_ARCH as required
+	$(Q) $(MAKE) -C $(UPNP_TOOLS)/scan
 
 $(UPNP_SCAN_TOOL):
-	$(Q) $(MAKE) -C $(UPNP_TOOLS)/scan -e 
+	$(info )
+	$(error Please run "make upnp-tools SMING_ARCH=Host")
 
 #
-UPNP_SCAN = $(UPNP_SCAN_TOOL) --nonet scan
-UPNP_PARSE = $(UPNP_SCAN_TOOL) --nonet parse
-UPNP_FETCH = $(UPNP_SCAN_TOOL) --nonet fetch
+UPNP_SCAN = $(UPNP_SCAN_TOOL) $(HOST_NETWORK_OPTIONS) -- scan $(HOST_PARAMETERS)
+UPNP_PARSE = $(UPNP_SCAN_TOOL) --nonet -- parse $(HOST_PARAMETERS)
+UPNP_FETCH = $(UPNP_SCAN_TOOL) --nonet fetch $(HOST_PARAMETERS)
+
+.PHONY: upnp-check-scan
+
+.PHONY: upnp-scan
+upnp-scan: $(UPNP_SCAN_TOOL) ##Run network scan (use HOST_PARAMETERS)
+	$(UPNP_SCAN)
+
+.PHONY: upnp-parse
+upnp-parse: $(UPNP_SCAN_TOOL) ##Parse raw device descriptions (use HOST_PARAMETERS)
+	$(UPNP_PARSE)
+
+.PHONY: upnp-fetch
+upnp-fetch: $(UPNP_SCAN_TOOL) ##Fetch device description by URL (use HOST_PARAMETERS)
+	$(UPNP_FETCH)
